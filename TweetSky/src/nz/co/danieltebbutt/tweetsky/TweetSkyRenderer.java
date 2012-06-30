0922ed7def365f76341634695630ef94627f46c4
Kazetsukai <moeruze.burning+git@gmail.com>
2012-06-30 19:50:23 +1200

    Added GLWallpaperService

---
+++ TweetSky/src/nz/co/danieltebbutt/tweetsky/TweetSkyRenderer.java
@@ -0,0 +1,28 @@
+package nz.co.danieltebbutt.tweetsky;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import net.rbgrn.android.glwallpaperservice.GLWallpaperService.Renderer;
+
+public class TweetSkyRenderer implements Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- TweetSky/src/nz/co/danieltebbutt/tweetsky/TweetSkyWallpaper.java
+++ TweetSky/src/nz/co/danieltebbutt/tweetsky/TweetSkyWallpaper.java
@@ -2,23 +2,18 @@ package nz.co.danieltebbutt.tweetsky;
 
 import java.util.ArrayList;
 
+import net.rbgrn.android.glwallpaperservice.GLWallpaperService;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Handler;
-import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
-public class TweetSkyWallpaper extends WallpaperService {
+public class TweetSkyWallpaper extends GLWallpaperService {
 
 	private final Handler mHandler = new Handler();
 
@@ -55,14 +50,6 @@ public class TweetSkyWallpaper extends WallpaperService {
 	}
 
 	class TweetSkyEngine extends Engine {
-
-		private final Paint mSkyPaint = new Paint();
-		private final Paint mCloudPaint = new Paint();
-		private float mWidth = 400;
-		private float mHeight = 300;
-
-		private ArrayList<Bitmap> mSkyBitmaps = new ArrayList<Bitmap>();
-		private ArrayList<Bitmap> mCloudBitmaps = new ArrayList<Bitmap>();
 		
 		private final Runnable mDrawRunnable = new Runnable() {
 			public void run() {
@@ -73,14 +60,10 @@ public class TweetSkyWallpaper extends WallpaperService {
 
 		TweetSkyEngine() {
 			mVisible = true;
-
-			mSkyPaint.setARGB(255, 255, 255, 255);
-			mCloudPaint.setARGB(255, 255, 255, 255);
 		}
 
 		@Override
 		public void onCreate(SurfaceHolder holder) {
-			holder.setFormat(PixelFormat.RGBA_8888);
 		}
 		
 		@Override
@@ -106,25 +89,6 @@ public class TweetSkyWallpaper extends WallpaperService {
 			mHeight = height;
 			mWidth = width;
 			
-			float biggestDimension = mHeight > mWidth ? mHeight : mWidth;
-			float ratio = 100 / biggestDimension;
-			
-			// Scale all bitmaps to the appropriate size for new resolution and cache
-			mSkyBitmaps.clear();
-			for (Drawable sky : mSkies) {
-				Bitmap bitmap = Bitmap.createScaledBitmap(((BitmapDrawable)sky).getBitmap(), (int)mWidth, (int)mHeight, false);
-				mSkyBitmaps.add(bitmap);
-			}			
-			mCloudBitmaps.clear();
-			for (Drawable cloud : mClouds) {
-				Bitmap bitmap = Bitmap.createScaledBitmap(
-									((BitmapDrawable)cloud).getBitmap(), 
-									(int)(cloud.getIntrinsicWidth() * ratio),
-									(int)(cloud.getIntrinsicHeight() * ratio),
-									false);
-				mCloudBitmaps.add(bitmap);
-			}
-			
 			drawFrame(false);
 		}
 
@@ -147,28 +111,6 @@ public class TweetSkyWallpaper extends WallpaperService {
 
 			if (doUpdate)
 				update();
-
-			final SurfaceHolder holder = getSurfaceHolder();
-
-			Canvas c = null;
-			try {
-				c = holder.lockCanvas();
-				if (c != null) {
-					Bitmap sky = mSkyBitmaps.get(0);
-					c.drawBitmap(sky, 0, 0, mSkyPaint);
-					
-					for (int i = 0; i < 4; i++) {
-						Bitmap cloud = mCloudBitmaps.get(i);
-						int x = i * 50;
-						int y = i * 100 % 230;
-						
-						c.drawBitmap(cloud, x, y, mCloudPaint);
-					}
-				}
-			} finally {
-				if (c != null)
-					holder.unlockCanvasAndPost(c);
-			}
 
 			// Reschedule the next redraw
 			mHandler.removeCallbacks(mDrawRunnable);
